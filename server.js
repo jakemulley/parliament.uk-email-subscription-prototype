430cb2544a0799d71a506bf16d50a34b41634d75
Jake Mulley <mulleyjb@parliament.uk>
2017-12-04 18:00:18 +0000

    General building

--- server.js
+++ server.js
@@ -2,19 +2,161 @@ const
 	express = require('express'),
 	app     = express(),
 	port    = process.env.PORT || 5000,
-	path    = require('path');
+	path    = require('path')
+	jsonFile= require(__dirname + '/src/json/topics.json');
+
+function properJson() {
+	var newJson = {
+		'ee': [],
+		'debates': [],
+		'accountability': [],
+		'research': [],
+		'news': [],
+		'bills': [],
+		'committees': []
+	};
+	for (var i = jsonFile.length - 1; i >= 0; i--) {
+		var done = false;
+		if(jsonFile[i].Visibility == 'Unlisted' || jsonFile[i].Visibility == 'Restricted') {
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Committee news') && done == false) {
+			newJson.news.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Speaker news') && done == false) {
+			newJson.news.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Twitter') && done == false) {
+			newJson.news.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Parliamentary Digital Service') && done == false) {
+			newJson.news.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Latest news from Parliament') && done == false) {
+			newJson.news.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Code.includes('Bill') && done == false) {
+			newJson.bills.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Committee') && done == false) {
+			newJson.committees.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Recess dates') && done == false) {
+			newJson.debates.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Westminster Hall Debates') && done == false) {
+			newJson.debates.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Prime Minister\'s Questions') && done == false) {
+			newJson.debates.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Adjournments Debates') && done == false) {
+			newJson.debates.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Question Book') && done == false) {
+			newJson.debates.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Votes and Proceedings') && done == false) {
+			newJson.debates.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Today\'s Written Statements') && done == false) {
+			newJson.debates.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Minutes of Proceedings') && done == false) {
+			newJson.debates.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Register of All-Party Groups') && done == false) {
+			newJson.debates.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('House of Commons Hansard') && done == false) {
+			newJson.debates.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Summary Agendas') && done == false) {
+			newJson.debates.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('expenses claims') && done == false) {
+			newJson.accountability.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Standards Allegations') && done == false) {
+			newJson.accountability.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Members\' Interests') && done == false) {
+			newJson.accountability.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Lords\' Interests') && done == false) {
+			newJson.accountability.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Briefing Papers') && done == false) {
+			newJson.research.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Debate Packs') && done == false) {
+			newJson.research.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('POST') && done == false) {
+			newJson.research.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Lords In Focus') && done == false) {
+			newJson.research.push(jsonFile[i]);
+			done = true;
+		}
+		if(jsonFile[i].Name.includes('Lords In Focus') && done == false) {
+			newJson.research.push(jsonFile[i]);
+			done = true;
+		}
+		if(done == false) {
+			newJson.ee.push(jsonFile[i]);
+		}
+	}
+	return newJson;
+}
 
 app.set('view engine', 'pug');
 app.set('views', './src/templates');
 app.use(express.static('_public'));
+
 app.get('/', function(req, res) {
 	res.render('index');
 });
+
 app.get('/confirmation', function(req, res) {
 	res.render('confirmation', { email: req.query.email });
 });
+
 app.get('/context', function(req, res) {
 	res.render('context');
+});
+
+app.get('/subscriptions', function(req, res) {
+	res.render('subscriptions');
+});
+
+app.get('/all-subscriptions', function(req, res) {
+	res.render('all-subscriptions', { data: properJson() });
 });
 
 app.listen(port, function() {
--- src/javascripts/overrides.js
+++ src/javascripts/overrides.js
@@ -1,3 +1,58 @@
 (function () {
-  console.log('Override javascript executed!');
+  // Sticky Nav Component
+  var Sticky = (function() {
+    'use strict';
+
+    var CSS_CLASS_ACTIVE = 'is-fixed';
+
+    var Sticky = {
+      element: null,
+      position: 0,
+      addEvents: function() {
+        window.addEventListener('scroll', this.onScroll.bind(this));
+      },
+      init: function(element) {
+        this.element = element;
+        this.addEvents();
+        this.position = element.offsetTop ;
+        this.onScroll();
+      },
+      aboveScroll: function() {
+        return this.position < window.scrollY;
+      },
+      onScroll: function(event) {
+        if (this.aboveScroll()) {
+          this.setFixed();
+        } else {
+          this.setStatic();
+        }
+      },
+      getWidth: function() {
+      	console.log(this.element.getBoundingClientRect());
+      	return this.element.getBoundingClientRect().width;
+      },
+      setFixed: function() {
+        this.element.classList.add(CSS_CLASS_ACTIVE);
+        // not needed if added with CSS Class
+        this.element.style.position = 'fixed';
+        this.element.style.top = 0;
+        this.element.style.width = this.getWidth();
+      },
+      setStatic: function() {
+        this.element.classList.remove(CSS_CLASS_ACTIVE);
+        // not needed if added with CSS Class
+        this.element.style.position = 'static';
+        this.element.style.top = 'auto';
+      }
+    };
+
+    return Sticky;
+
+  })();
+
+
+  //  Init Sticky
+  var sticky = document.querySelector('.sticky');
+  if (sticky)
+    Sticky.init(sticky);
 })();
